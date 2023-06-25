0365d999961054e0326fc6c8a9746e50b9df9217
Bournane Mohamed <mbournane@gmail.com>
2023-06-25 13:18:01 +0200

    programe delegate

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Delegate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShowMoney rising = RisingEuro;
+        rising(1500);
+
+        rising = RisingDollars;
+        rising(1500);
+    }
+    public delegate void ShowMoney(double rising);
+    public static void RisingEuro(double rising)
+    {
+        Console.WriteLine(rising+" euros");
+    }
+    public static void RisingDollars(double rising)
+    {
+        Console.WriteLine(rising +" dollars");
+    }
+}
